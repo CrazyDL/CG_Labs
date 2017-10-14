436710a32a7854d2010b65e82ea46f3a90682607
CrazyDL <dlevshtanov@mail.ru>
2017-10-14 18:57:35 +0300

    Chenged path

--- CG_Lab2/app/src/main/java/com/crazydl/cg_lab2/OrthogrProj.java
+++ CG_Lab2/app/src/main/java/com/crazydl/cg_lab2/OrthogrProj.java
@@ -4,34 +4,41 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.renderscript.Matrix3f;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
 public class OrthogrProj extends View{
     Paint pFigure;
     Path path;
-
     OctagonalPrism octPrism;
+    Matrix3f matrix3f;
 
     private ScaleGestureDetector scaleGestureDetector;
     private GestureDetector gestureDetector;
 
-    private static float viewWidth, viewHeight;
+    private static float offsetX = 0;
+    private static float offsetY = 0;
+    private static float scale = 1f;
 
     public OrthogrProj(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public OrthogrProj(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
-    private void init(){
+    private void init(Context context){
+        scaleGestureDetector = new ScaleGestureDetector(context, new MyScaleGestureListener());
+        gestureDetector = new GestureDetector(context, new MyGestureListener());
+
         octPrism = new OctagonalPrism();
 
         pFigure = new Paint();
@@ -43,16 +50,36 @@ public class OrthogrProj extends View{
         pFigure.setStrokeJoin(Paint.Join.ROUND);
         pFigure.setStrokeCap(Paint.Cap.ROUND);
         pFigure.setAntiAlias(true);
+
+        matrix3f = new Matrix3f();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        viewWidth = canvas.getWidth();
-        viewHeight = canvas.getHeight();
+        float viewWidth = canvas.getWidth();
+        float viewHeight = canvas.getHeight();
         canvas.drawColor(ContextCompat.getColor(getContext(), R.color.colorCanvasBackground));
-        //canvas.save();
+
         canvas.translate(viewWidth / 2, viewHeight / 2);
-        for (int i = 0; i < OctagonalPrism.EDGES; i++){
+        matrix3f.scale(scale, scale, scale);
+
+        path.reset();
+        for (int i = 0; i < OctagonalPrism.EDGES - 1; i++){
+            path.moveTo(octPrism.vrts[0][i].getX(), octPrism.vrts[0][i].getY());
+            path.lineTo(octPrism.vrts[0][i + 1].getX(), octPrism.vrts[0][i + 1].getY());
+            path.lineTo(octPrism.vrts[1][i + 1].getX(), octPrism.vrts[1][i + 1].getY());
+            path.lineTo(octPrism.vrts[1][i].getX(), octPrism.vrts[1][i].getY());
+            path.lineTo(octPrism.vrts[0][i].getX(), octPrism.vrts[0][i].getY());
+        }
+        path.moveTo(octPrism.vrts[0][OctagonalPrism.EDGES - 1].getX(), octPrism.vrts[0][OctagonalPrism.EDGES - 1].getY());
+        path.lineTo(octPrism.vrts[0][0].getX(), octPrism.vrts[0][0].getY());
+        path.moveTo(octPrism.vrts[1][OctagonalPrism.EDGES - 1].getX(), octPrism.vrts[1][OctagonalPrism.EDGES - 1].getY());
+        path.lineTo(octPrism.vrts[1][0].getX(), octPrism.vrts[1][0].getY());
+
+        canvas.drawPath(path, pFigure);
+
+
+        /*for (int i = 0; i < OctagonalPrism.EDGES; i++){
             canvas.drawLine(octPrism.vrts[0][i].getX(), octPrism.vrts[0][i].getY(),
                     octPrism.vrts[1][i].getX(), octPrism.vrts[1][i].getY(), pFigure);
         }
@@ -64,8 +91,44 @@ public class OrthogrProj extends View{
             }
             path.lineTo(octPrism.vrts[k][0].getX(), octPrism.vrts[k][0].getY());
             canvas.drawPath(path, pFigure);
-        }
-        //canvas.restore();
+        }*/
+
         invalidate();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        scaleGestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    private class MyScaleGestureListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float newScale = scale * scaleGestureDetector.getScaleFactor();
+            if(newScale > 0.4 && newScale < 2)
+                scale = newScale;
+            return true;
+        }
+    }
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            offsetX -= distanceX;
+            offsetY -= distanceY;
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent event){
+            scale = 1f;
+            offsetX = 0;
+            offsetY = 0;
+            return true;
+        }
     }
 }
